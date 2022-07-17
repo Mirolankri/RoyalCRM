325a8272191dde2ccb9e4bb0d095be7a38c9cc43
KrakovBen <93730442+KrakovBen@users.noreply.github.com>
2022-07-17 23:42:01 +0300

    HW

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './routes/app-routing.module';
+import { AppComponent } from './layout/app.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
